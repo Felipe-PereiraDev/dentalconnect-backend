039421a1e61db078fe4055ad44076b95b0a8f80a
Felipe-PereiraDev <felipe.pereirasilvaa@outlook.com>
2026-01-03 22:03:12 -0400

    first commit

---
+++ src/main/java/com/github/felipe_pereiradev/dentalconnect/DentalconnectApplication.java
@@ -0,0 +1,13 @@
+package com.github.felipe_pereiradev.dentalconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/felipe_pereiradev/dentalconnect/DentalconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felipe_pereiradev.dentalconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
